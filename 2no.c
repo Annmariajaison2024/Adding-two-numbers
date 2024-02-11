e17f435151fe5eb5b95f34a8afd24dc274cd908c
Ann Maria Jaison <158474407+Annmariajaison2024@users.noreply.github.com>
2024-02-11 10:59:38 +0530

    Add files via upload

---
+++ 2no.c
@@ -0,0 +1,11 @@
+//2no.c
+#include<stdio.h>
+void main()
+	{ int a,b,x;
+		printf("Enter first integer:");
+		scanf("%d", &a);
+		printf("Enter second integer:");
+		scanf("%d", &b);
+		x= a+b;
+		printf("The sum of these two integers are %d \n",x);
+	}
